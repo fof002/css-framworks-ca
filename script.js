81d1b80ffce1bb36d14d1baaa9e1ba7023fc210c
fof002 <80104002+fof002@users.noreply.github.com>
2023-03-15 12:33:59 +0100

    Added minLength

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
 const loginForm = document.querySelector("#login-form");
-const passwordInput = document.querySelector("#password");
-const errorcontainer = document.querySelector("#error");
 
 loginForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  checkLength(8);
 });
-
-function checkLength(len) {
-  if (passwordInput.value.trim().length < len) {
-    return (errorcontainer.innerHTML =
-      "Your password needs to be at least 8 characters");
-  } else {
-    errorcontainer.innerHTML = "";
-  }
-}
