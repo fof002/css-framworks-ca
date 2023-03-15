27cb009a4dfbc2a987bf33e939d62aaad14e5541
fof002 <80104002+fof002@users.noreply.github.com>
2023-03-15 13:31:05 +0100

    updated function

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const loginForm = document.querySelector("#login-form");
+const emailInput = document.querySelector("#userName");
+const errorcontainer = document.querySelector("#error");
+const passwordInput = document.querySelector("#password");
+const submitButton = document.querySelector("#submit");
 
-loginForm.addEventListener("submit", (event) => {
+submitButton.addEventListener("submit", (event) => {
   event.preventDefault();
+  if (!emailInput.value || passwordInput.value.trim().length < 8) {
+    return (errorcontainer.innerHTML =
+      "You need to have a valid noroff email and a password longer than 8 characters");
+  } else {
+    errorcontainer.innerHTML = "";
+  }
 });
